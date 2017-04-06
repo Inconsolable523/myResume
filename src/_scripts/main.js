4fbad751b80129828208aa46dab91a7b891a45a3
Inconsolable523 <1273258990@qq.com>
2017-04-06 10:38:03 +0800

    fix(browser): information fix

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -9,6 +9,6 @@
   bd.src = "//hm.baidu.com/hm.js?f940aae7b3f51d3eae76f0fe4ec1bf27";
   doc.body.appendChild(bd);
 
-  console.log('😊 Welcome to Zhi\'s resume!');
+  console.log('😊 Welcome to ZhiChao\'s resume!');
   console.log('The resume source code is on https://github.com/wssgcg1213/resume');
 })(document);
